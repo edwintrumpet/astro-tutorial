f3ac05db6c03084f6371a50b2a8863ae4c044b2d
edwin garcia <iam.edwin.code@gmail.com>
2025-08-23 23:02:37 -0500

    add preact component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "http://edwin-garcia-astro-tutorial.netlify.app/"
+  site: "http://edwin-garcia-astro-tutorial.netlify.app/",
+  integrations: [preact()]
 });
